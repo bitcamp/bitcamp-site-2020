8f80ec497b5edfaaa501e4992990e90d647a1eb1
Evan McIntire <mcintire.evan@gmail.com>
2020-02-10 22:12:57 -0500

    footer

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 function showNav() {
-    document.getElementById('mobile-hamburger').style.display = 'none';
-    document.getElementById('mobile-nav').style.display = 'flex';
-    document.getElementById('mobile-nav-close').style.display = 'block';
-    document.getElementById('social').style.display = 'none';
+  document.getElementById("mobile-hamburger").style.display = "none";
+  document.getElementById("mobile-nav").style.display = "flex";
+  document.getElementById("mobile-nav-close").style.display = "block";
+  document.getElementById("social").style.display = "none";
 }
 
 function hideNav() {
-    document.getElementById('mobile-hamburger').style.display = 'block';
-    document.getElementById('mobile-nav').style.display = 'none';
-    document.getElementById('mobile-nav-close').style.display = 'none';
-    document.getElementById('social').style.display = 'block';
+  document.getElementById("mobile-hamburger").style.display = "block";
+  document.getElementById("mobile-nav").style.display = "none";
+  document.getElementById("mobile-nav-close").style.display = "none";
+  document.getElementById("social").style.display = "block";
+}
+
+function showFooter() {
+  document.getElementById("footer-links").style.display = "inline-flex";
+  document.getElementById("footer-emails").style.display = "inline-flex";
+
+  document.getElementById("footer-expand-up").style.display = "none";
+  document.getElementById("footer-expand-down").style.display = "flex";
+}
+
+function hideFooter() {
+  document.getElementById("footer-links").style.display = "none";
+  document.getElementById("footer-emails").style.display = "none";
+
+  document.getElementById("footer-expand-up").style.display = "flex";
+  document.getElementById("footer-expand-down").style.display = "none";
 }
