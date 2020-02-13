2d5d2544126a06be443332854925a2423163b892
Evan McIntire <mcintire.evan@gmail.com>
2020-02-13 15:21:22 -0500

    desktop modal

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.addEventListener("keydown", event => {
+  if (event.isComposing || event.keyCode === 27) {
+    hideModal();
+  }
+});
+
 function showNav() {
   document.getElementById("mobile-hamburger").style.display = "none";
   document.getElementById("mobile-nav").style.display = "flex";
@@ -26,4 +32,14 @@ function hideFooter() {
 
   document.getElementById("footer-expand-up").style.display = "flex";
   document.getElementById("footer-expand-down").style.display = "none";
+}
+
+function showModal() {
+  document.getElementById("body").style.overflow = "hidden";
+  document.getElementById("modal-bg").style.display = "flex";
+}
+
+function hideModal() {
+  document.getElementById("body").style.overflow = "auto";
+  document.getElementById("modal-bg").style.display = "none";
 }
