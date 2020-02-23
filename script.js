08a0c0c80facb80849004da1e9a72ef490e6ef3c
Evan McIntire <mcintire.evan@gmail.com>
2020-02-23 15:02:18 -0500

    Update nav

--- script.js
+++ script.js
@@ -5,17 +5,13 @@ document.addEventListener("keydown", event => {
 });
 
 function showNav() {
-  document.getElementById("mobile-hamburger").style.display = "none";
   document.getElementById("mobile-nav").style.display = "flex";
   document.getElementById("mobile-nav-close").style.display = "block";
-  document.getElementById("social").style.display = "none";
 }
 
 function hideNav() {
-  document.getElementById("mobile-hamburger").style.display = "block";
   document.getElementById("mobile-nav").style.display = "none";
   document.getElementById("mobile-nav-close").style.display = "none";
-  document.getElementById("social").style.display = "block";
 }
 
 function showFooter() {
